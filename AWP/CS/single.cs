6bc9a16b3a17c68e533550e1af6eeeff9198564a
123nush <119521757+123nush@users.noreply.github.com>
2023-08-20 18:11:18 +0530

    Update single.cs

--- AWP/CS/single.cs
+++ AWP/CS/single.cs
@@ -28,3 +28,36 @@ public class Demo
         e1.empl_info(20000);
     }
 }
+
+
+2nd way using constructor
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+class person{
+    int age;
+    string name;
+    public person(int a,string n){
+        age=a;
+        name=n;
+        Console.WriteLine("Name is:"+name+"\n"+"Age:"+age);
+    }
+}
+class employee:person{
+    int salary;
+    public employee(int a,string n,int s):base(a,n){
+    
+        salary=s;
+         Console.WriteLine("Salary is:"+salary);
+    }
+}
+
+public class Demo
+{
+    public static void Main(string[] args)
+    {
+        employee e1=new employee(20,"Anu",2000000);
+        // e1.empl_info(20000);
+    }
+}
