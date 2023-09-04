0497c371a43471955da3e1a635464231f5e58cf1
123nush <119521757+123nush@users.noreply.github.com>
2023-09-04 09:02:42 +0530

    Update deck_long_version.py

--- AI/deck_long_version.py
+++ AI/deck_long_version.py
@@ -1,18 +1,19 @@
 #first let's import random procedures since we will be shuffling
 import random
-#next, let's start building list holders so we can place our cards in there:
-cardfaces = []
+cardfaces = []#used to store  single set of cards
 suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 royals = ["J", "Q", "K", "A"]
-deck = []
+deck = []#used to store all set of cards
 #now, let's start using loops to add our content:
 for i in range(2,11):
     cardfaces.append(str(i)) #this adds numbers 2-10 and converts them to string data
 for j in range(4):
     cardfaces.append(royals[j]) #this will add the royal faces to the cardbase
-for k in range(4):
-    for l in range(13):
+#print(cardfaces)
+for k in range(4):#loop to take name of each suit
+    for l in range(13):#loop to create set of each suit 
         card = (cardfaces[l] + " of " + suits[k])
+        #print(card) to get complete one set before shuffling
 #this makes each card, cycling through suits, but first through faces
         deck.append(card)
 #this adds the information to the "full deck" we want to make
