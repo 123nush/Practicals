4ec7fc68201526bab07acf8367f6e50b122090d4
123nush <119521757+123nush@users.noreply.github.com>
2023-09-05 10:19:05 +0530

    Update Prac2a.cs

--- AWP/CS/Prac2a.cs
+++ AWP/CS/Prac2a.cs
@@ -11,10 +11,39 @@ fact=fact*i;
 }
 Console.WriteLine("Factorial of the number:"+fact);
 }
+public static void temerature(){
+    Console.WriteLine("Enter Temperature in celcius or fahreneit:");
+    string temp=Console.ReadLine();
+    if(temp.Contains("F")){
+        string tem=temp.Trim('F');
+        double t=Convert.ToDouble(tem);
+       double conversion;
+       Console.WriteLine(t);
+        conversion=(t-32)*(5.0/9.0);
+        Console.WriteLine("temperature in celcius after conversion:{0}",conversion);
+    }
+    else{
+         string tem=temp.Trim('C');
+        double t=Convert.ToDouble(tem);
+       double conversion;
+        conversion=(t)*(9/5)+32;
+        Console.WriteLine("temperature in fahreneit after conversion:"+ conversion);
+
+    }
+
+}
 public static void Main(String []args){
 factorial();
+temerature();
 
 }
-
-
 }
+
+//output
+Enter a number:
+5
+Factorial of the number:120
+Enter Temperature in celcius or fahreneit:
+132 F
+132
+temperature in celcius after conversion:55.5555555555556
