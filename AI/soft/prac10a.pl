bea275e9bf231f2ff2bcca7b0de58277f315d5d8
123nush <119521757+123nush@users.noreply.github.com>
2023-09-09 10:44:28 +0530

    Create prac10a.pl

---
+++ AI/soft/prac10a.pl
@@ -0,0 +1,25 @@
+
+
+cricketer(sachin).
+batsman(cricketer).
+
+
+check(Name,Profession):-
+batsman(Profession),
+cricketer(Name).
+
+
+1 ?- check(hardik,cricketer).
+false.
+
+2 ?- check(hardik,cricketer).
+false.
+
+3 ?- check(sachin,cricketer).
+true.
+
+4 ?- check(A,cricketer).
+A = sachin.
+
+5 ?- check(sachin,B).
+B = cricketer.
